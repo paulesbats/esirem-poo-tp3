21a725a2d56e003bf3d0de6d4573e58818894f5e
Adrien <a.bonnand@pacman.fr>
2024-03-25 16:02:12 +0100

    Rectification CardList(use list)

--- src/CardList.java
+++ src/CardList.java
@@ -1,22 +1,11 @@
 public class CardList {
 
-    private String value;
-    private CardList cardNext;
+    private Card[] cardlist;
 
-    public String getValue(){
-        return this.value;
+
+    public Card[] getCardlist(){
+        return this.cardlist;
     }
 
-    public CardList getCardNext(){
-        return this.cardNext ;
-    }
-
-    public void setValue(String _value){
-        this.value=_value;
-    }
-
-    public void setCardNext(CardList _cardnext){
-        this.cardNext=_cardnext;
-    }
 
 }
