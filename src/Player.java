8ea2e1d56cd989c5efa0111c76c9746c438ba544
AlbertSMEETS <albert.smeets@laposte.net>
2024-03-27 15:58:55 +0100

    Including a while in the show_hand fonction, trying to optimise the code.

--- src/Player.java
+++ src/Player.java
@@ -28,33 +28,39 @@ public class Player {
         int i = 0;
         int size = Hand.size();
 
+        Card newCard = Hand.get(0);
+        while(newCard!=null){
 
-        Card firstCard = Hand.get(0);
-        System.out.println("Voici la carte 1 :\n");
-        switch (firstCard.getType())
-        {
-            case "clubs":
-                System.out.println("Color : CLUBS | "); ;
-                break;
-            case "spades":
-                System.out.println("Color : SPADES | ") ;
-                break;
-            case "diamonds":
-                System.out.println("Color : DIAMONDS | ") ;
-                break;
-            case "hearts":
-                System.out.println("Color : HEARTS | ") ;
-                break;
+            //Card firstCard = Hand.get(0);
+            System.out.println("Voici la carte 1 :\n");
+            switch (newCard.getType()) {
+                case "clubs":
+                    System.out.println("Color : CLUBS | ");
+                    ;
+                    break;
+                case "spades":
+                    System.out.println("Color : SPADES | ");
+                    break;
+                case "diamonds":
+                    System.out.println("Color : DIAMONDS | ");
+                    break;
+                case "hearts":
+                    System.out.println("Color : HEARTS | ");
+                    break;
+            }
+            if (newCard.getValue() <= 10) {
+                System.out.println("Value = " + newCard.getValue());
+            } else if (newCard.getValue() == 11) {
+                System.out.println("Value = JACK");
+            } else if (newCard.getValue() == 12) {
+                System.out.println("Value = QUEEN");
+            } else {
+                System.out.println("Value = KING");
+            }
+            i++;
+            newCard = Hand.get(i);
         }
-        if(firstCard.getValue() <= 10){
-            System.out.println("Value = " + firstCard.getValue());
-        } else if (firstCard.getValue() == 11) {
-            System.out.println("Value = JACK" );
-        } else if (firstCard.getValue() == 12) {
-            System.out.println("Value = QUEEN" );
-        } else {
-            System.out.println("Value = KING");
-        }
+
     }
 
 }
