603f99b296b244ba518acae776d97b5f6d10cd83
Paulo <paullesbats2003@gmail.com>
2024-03-27 14:48:04 +0100

    Clean Main

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Hello and welcome!");
         /*  test deck display
         Player deck=new Player();
 
         deck.createDeck();
 
         for (int i = 0; i < 52; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println(deck.getHand().get(i).getValue());
             System.out.println(deck.getHand().get(i).getType());
         }
         deck.shuffleDeck();
 
         for (int i = 0; i < 52; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println(deck.getHand().get(i).getValue());
             System.out.println(deck.getHand().get(i).getType());
         }
