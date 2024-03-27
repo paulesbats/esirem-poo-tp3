6e0aff6fa55d0750ba87a458cf73a0fc26ca6d3a
Paulo <paullesbats2003@gmail.com>
2024-03-27 16:10:23 +0100

    Clean Main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
-        /*  test deck display
-        Player deck=new Player();
 
+        Player deck=new Player();
         deck.createDeck();
 
         for (int i = 0; i < 52; i++) {
@@ -18,6 +14,5 @@ public class Main {
             System.out.println(deck.getHand().get(i).getValue());
             System.out.println(deck.getHand().get(i).getType());
         }
-        */
     }
 }
