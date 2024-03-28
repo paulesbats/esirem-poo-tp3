058bf13d4959e13bb6439dade0c4267b3c063f40
Adrien Bonnand <104890399+Bonnand@users.noreply.github.com>
2024-03-28 17:17:23 +0100

    Improvement of round function (#18)
    Co-authored-by: Adrien <a.bonnand@pacman.fr>

--- src/Game.java
+++ src/Game.java
@@ -1,23 +1,29 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Game {
 
 
     private int bet;
 
-    public Game(int _bet){
-        this.bet=_bet;
-    }
-    public Game(){}
-
-
-    public void setBet(int _bet){
-        this.bet=_bet;
+    public Game(int _bet) {
+        this.bet = _bet;
     }
 
-    public int getBet(){
+    public Game() {
+    }
+
+
+    public void setBet(int _bet) {
+        this.bet = _bet;
+    }
+
+    public int getBet() {
         return this.bet;
     }
 
-    public void firstDraw(Player deck,Player player, Player bank){
+    public void firstDraw(Player deck, Player player, Player bank) {
         Card card = deck.cardDraw();
         player.addCardtoHand(card);
         card = deck.cardDraw();
@@ -27,46 +33,80 @@ public class Game {
 
     }
 
-    public void giveaCard(Player deck, Player player){
+    public void giveaCard(Player deck, Player player) {
         Card card = deck.cardDraw();
         player.addCardtoHand(card);
     }
 
-    public void winner(Player player, Player bank){
+    public void winner(Player player, Player bank) {
         /* Points are counted as in the Wikipedia documentation only the blackjack columns and lines are removed*/
 
-        if(player.getPlayerPoint()==bank.getPlayerPoint()){
+        if (player.getPlayerPoint() == bank.getPlayerPoint()) {
             System.out.println("equality");
-        }
-        else if(player.getPlayerPoint()>bank.getPlayerPoint()){
+        } else if (player.getPlayerPoint() > bank.getPlayerPoint()) {
             System.out.println("Player win the game");
             player.addPlayerAmount(bet);
-        }
-        else{
+        } else {
             System.out.println("Bank win the game");
             player.addPlayerAmount(-(bet));
         }
     }
 
-    public static void round(){
+    public void displayShowHand(Player player,Player bank){
+        System.out.println("-----------------------------------------------------------");
+        player.showHand();
+        System.out.println("-----------------------------------------------------------");
+        bank.showHand();
+        System.out.println("-----------------------------------------------------------");
+    }
 
-        Game game= new Game(0);
+    public static void round() {
 
-        Player player=new Player();
-        Player bank=new Player();
-        Player deck=new Player();
+        Game game = new Game(0);
+        Player player = new Player();
+        Player bank = new Player();
+        Player deck = new Player();
+
+        BufferedReader console;
+        console = new BufferedReader(new InputStreamReader(System.in));
 
         player.setAmount(100);
+        String choice = "";
 
 
         deck.createDeck();
         deck.shuffleDeck();
+        game.firstDraw(deck, player, bank);
 
-        game.firstDraw(deck,player,bank);
+        game.displayShowHand(player,bank);
 
-        player.showHand();
-        System.out.println("-----------------------------------------------------------");
-        bank.showHand();
+        System.out.println("What do you want do ? Hit:h | Stand:st | Surrender:su");
+        try {
+            choice = console.readLine();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        player.CountPoint();
+
+        while (choice.equals("h") && player.getPlayerPoint()<21 && player.getPlayerPoint()!=-4) {
+
+            player.addCardtoHand(deck.cardDraw());
+            game.displayShowHand(player,bank);
+
+
+            System.out.println("What do you want do ? Hit:h | Stand:st | Surrender:su");
+            try {
+                choice = console.readLine();
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+            player.CountPoint();
+        }
+        System.out.println(player.getPlayerPoint());
+        game.displayShowHand(player,bank);
 
 
     }
